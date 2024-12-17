03f94dda48ffc2ebb5b858b56e9438aeff069b9a
Ramazan Güner <rguner@gmail.com>
2024-12-17 11:31:19 +0300

    object

--- src/main/java/com/guner/kafka/service/KafkaReceiver.java
+++ src/main/java/com/guner/kafka/service/KafkaReceiver.java
@@ -18,7 +18,7 @@ public class KafkaReceiver {
     @KafkaListener(topics = "topic-1", groupId = "group-2")
     public void listenGroupGroup2(String message) {
         log.info("-----   Received Message in group group-2: {}", message);
-        throw new RuntimeException("Receive Exception to test retry mechanism");
+        //throw new RuntimeException("Receive Exception to test retry mechanism");
     }
 
     //@KafkaListener(topics = "topic-1, topic-2", groupId = "group-1")
@@ -31,7 +31,7 @@ public class KafkaReceiver {
     @KafkaListener(topics = "topic-greeting", containerFactory = "jsonKafkaListenerContainerFactory")
     public void jsonObjectListener(Object object) {
         Greeting greeting = (Greeting) ((ConsumerRecord)object).value();
-        log.info("-----   Received Greeting Message on topic-2 in group group-1: {}", object);
+        log.info("-----   Received Object Message on topic-2 in group group-1: {}", object);
         log.info("-----   Received Greeting Message on topic-2 in group group-1: {}", greeting);
 
     }
